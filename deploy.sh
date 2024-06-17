a1b72ab122b25dd24b76b806fc052827b48eb467
komekami <komekami0131@gmail.com>
2024-06-18 08:04:49 +0900

    chore: deployスクリプトの追加

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dotfiles=(.zshrc tmux/tmux.conf)
+
+for file in "${dotfiles[@]}"; do
+    ln -svf ~/dotfiles/$file ~/
+done
+
+# ~/.config/nvimが存在しない場合にのみシンボリックリンクを作成する
+if [ ! -e ~/.config/nvim ]; then
+    ln -svf ~/dotfiles/nvim ~/.config/nvim
+else
+    echo "~/.config/nvim already exists, skipping symlink creation."
+fi
